727a530ab37d1a795a46cb5482062dd33798f658
Mateusz Switala <mswitala@redhat.com>
2026-03-18 15:27:08 +0100

    fix import path in autorag integration tests
    Signed-off-by: Mateusz Switala <mswitala@redhat.com>

--- pipelines/training/autorag/documents_lite_rag_optimization_pipeline/tests/test_pipeline_integration.py
+++ pipelines/training/autorag/documents_lite_rag_optimization_pipeline/tests/test_pipeline_integration.py
@@ -19,7 +19,7 @@ import pytest
 
 def _skip_if_no_lite_integration_config():
     """Return True if integration config is not set (skip test)."""
-    from integration_config import DOCRAG_LITE_INTEGRATION_CONFIG
+    from .integration_config import DOCRAG_LITE_INTEGRATION_CONFIG
 
     return DOCRAG_LITE_INTEGRATION_CONFIG is None
 
